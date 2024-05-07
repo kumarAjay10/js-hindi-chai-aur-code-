30d011d1e380e62f16b9e385c9b0358b65acbc2e
kumarAjay10 <pukaleajay@gmail.com>
2024-05-07 12:29:02 +0530

    variables

---
+++ 01-variables.js
@@ -0,0 +1,19 @@
+// variables 
+const accountId = 144562;
+let accountEmail = "Ajaykumar@gmail.com";
+var accountPassword = "23455456";
+accountCity = "jaipur";
+
+// accountId = 2; // Assignment to constant variables
+// console.log(accountId);
+
+accountEmail ="xyz@gmail.com";
+accountPassword = "6543";
+accountCity = "bengaluru";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
