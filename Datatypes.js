34af6e185e71f6445af4dff8cc0f8a7f8a358071
kumarAjay10 <pukaleajay@gmail.com>
2024-05-07 13:15:42 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,25 @@
+/*
+Datatypes
+1. numbers
+2.strings
+3.boolean
+4.undefined
+5.null
+6.bigInt
+7. symbol
+*/
+
+let name = "Ajay";
+let age = 23;
+let loggedIn = true;
+let state;
+let empty = null;
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof loggedIn);
+console.log(typeof state);
+console.log(typeof empty); // object
+
+// alert("hello"); we are using Node.js not browser we will learn in upcoming videos 
