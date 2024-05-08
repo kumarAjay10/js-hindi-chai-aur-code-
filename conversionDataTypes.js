dd5cd6e49a991da5bb8c9740144ff7521e457180
kumarAjay10 <pukaleajay@gmail.com>
2024-05-08 11:09:43 +0530

    conversionDatatypes

---
+++ conversionDataTypes.js
@@ -0,0 +1,29 @@
+// Datatype conversion
+
+let score = null//true// undefined//null //"33abc"  // 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 -> number 
+// "33abc" -> NaN
+// true -> 1 ; false -> 0
+// undefiend -> NaN
+// null -> 0
+
+
+// conversion in boolean
+let isLoggedIn = " ";       //"";   //1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// conversion in string
+
+let someNumber = 33;
+let stringNUmber = String(someNumber);
+console.log(typeof stringNUmber);
