642618c62fea2f7c759c3adb38521c2eb43e842a
kumarAjay10 <pukaleajay@gmail.com>
2024-05-18 15:52:34 +0530

    loops

---
+++ 12-looops.js
@@ -0,0 +1,108 @@
+
+// synatx for loop
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// for loop example
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+
+// for (let i = 1; i < 10; i++) {
+//     const element = i;
+   
+// }
+// console.log(element); cannot access outside loop 
+
+// for (let i = 0; i <=10; i++) {
+//     const element = i; 
+//     if(element == 5)  {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+// nested loop
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//    for (let j = 0; j <=10; j++) {
+//   console.log(`inner loop value: ${j} and inner loop ${i}`);
+//    } 
+// }
+
+
+
+ //nested loop and tables 
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//    for (let j = 0; j <=10; j++) {
+//   console.log(i + '*' + j + '=' + i*j) ;
+//    } 
+// }
+
+// using array 
+
+// let myArray = ["flash","batman","superman"];
+// console.log(myArray.length);
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+// break
+// for (let i = 0; i <=20 ; i++) {
+//    if(i==5){
+//     console.log('detected 5');
+//     break;
+//    }
+    
+//    console.log(`value of ${i}`);
+// }
+
+// continue
+// for (let i = 0; i <=20 ; i++) {
+//     if(i==5){
+//      console.log('detected 5');
+//      continue;
+//     }
+     
+//     console.log(`value of ${i}`);
+//  }
+
+
+// while loop 
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// using array
+// let myArray = ["flash","Batman","superman"];
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+
+// do while loop 
+// let score = 1;
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+// let score = 11;
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <= 10);
+   
